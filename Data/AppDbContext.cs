71f989400f8e3ffb35fa71e70b5428d48b2c98db
Agung Januardi <agung.januardi@bsi.co.id>
2020-03-02 17:09:29 +0700

    test

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShortUrl.Model;
+
+namespace ShortUrl.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Url> Urls { get; set; }
+        public AppDbContext(DbContextOptions options) : base (options)
+        {
+        }
+    }
+}
---
+++ Migrations/20200302072915_database.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ShortUrl.Migrations
+{
+    public partial class database : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Urls",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    title = table.Column<string>(nullable: true),
+                    shorturl = table.Column<string>(nullable: true),
+                    userid = table.Column<int>(nullable: false),
+                    createdat = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Urls", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(nullable: true),
+                    email = table.Column<string>(nullable: true),
+                    password = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Urls");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
